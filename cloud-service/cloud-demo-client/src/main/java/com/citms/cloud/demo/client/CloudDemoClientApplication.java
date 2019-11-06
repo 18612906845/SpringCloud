72cc0f8a0992265ec895c786b79961db2483c4b6
wanglei <wanglei@citms.cn>
2019-11-06 19:07:01 +0800

    增加demo服务和客户端模块

---
+++ cloud-service/cloud-demo-client/src/main/java/com/citms/cloud/demo/client/CloudDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.citms.cloud.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDemoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoClientApplication.class, args);
+    }
+
+}
---
+++ cloud-service/cloud-demo-server/src/main/java/com/citms/cloud/demo/server/CloudDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.citms.cloud.demo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoServerApplication.class, args);
+    }
+
+}
